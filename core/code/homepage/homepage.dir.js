b8d44519ba553d8cb8a1969828a465724d23d632
Giovanni Coppola <giovanni.skate.93@hotmail.it>
2020-10-23 20:09:48 +0200

    fix names

--- core/code/homepage/homepage.dir.js
+++ core/code/homepage/homepage.dir.js
@@ -5,15 +5,15 @@ import "../../images/favicon.ico"; //TO BE DELETED
 import "../../images/pulvs.gif"; //TO BE DELETED
 import "../../images/contacts/instagram.png"; //TO BE DELETED
 import "../../images/contacts/twitter.png"; //TO BE DELETED
-import "../../images/projects-images/sit down please.gif"; //TO BE DELETED
+import "../../images/projects-images/sit-down-please.gif"; //TO BE DELETED
 
 export default(function() {
     return {
         templateUrl: "/templates/homepage-tpl.html",
-        controller: ["$scope", "$window", "$document", function($scope, $window, $document) {
+        controller: ["$scope", "$window", "$document", function($scope, $window) {
             $scope.projects = [{
-                name: "Sit Down, Please",
-                path: "/images/sit down please.gif"
+                name: "Sit Down, Pleaseee",
+                path: "/images/sit-down-please.gif"
             }];
 
             $scope.openPage = function(url) {
